6ee2baaab6e06d7c46a86a65c8a2b9b98df1c276
mtorcellini <matt.torcellini@gmail.com>
2021-06-01 16:30:22 -0400

    initial layout for projects page

--- src/components/pages/Projects/index.js
+++ src/components/pages/Projects/index.js
@@ -1,6 +1,34 @@
 function Projects() {
   return (
-    <h1>Projects</h1>
+    <>
+      <h1 className="title is-1">Projects</h1>
+      <div className="columns">
+        <div className="column is-half">
+          <div className="box">
+            <h3 className="title is-3">Jubilee Austen</h3>
+            <p className="subtitle">A single page portfolio thing</p>
+            <div className="tags">
+              <span className="tag">HTML5</span>
+              <span className="tag">CSS3</span>
+            </div>
+          </div>
+        </div>
+        <div className="column is-half">
+          <div className="box">
+            <h3 className="title is-3">eCommerce Backend</h3>
+            <p className="subtitle">Backend for an eCommerce application</p>
+            <div className="tags">
+              <span className="tag">Node.js</span>
+              <span className="tag">MySQL</span>
+              <span className="tag">Express</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+
+
+
   )
 }
 
