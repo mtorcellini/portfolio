c84e2a3afbb685f5da919bbc36378fcd09f45af4
mtorcellini <matt.torcellini@gmail.com>
2021-06-01 13:15:12 -0400

    setup file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Nav from './components/Nav';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Router>
+        <Route exact path="/" component={About} />
+        <Route path="/projects" component={Projects} />
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,9 @@
+import {Link, useLocation} from 'react-router-dom';
+
+function Nav() {
+  return (
+    <h1>Nav</h1>
+  )
+}
+
+export default Nav;
