24e0e280bdad386fea200022813cd0388cc0ec05
mtorcellini <matt.torcellini@gmail.com>
2021-06-11 17:33:53 -0400

    add basename to react router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Nav />
         {/* <Route exact path="/" component={About} /> */}
         <Route path="/projects" component={Projects} />
